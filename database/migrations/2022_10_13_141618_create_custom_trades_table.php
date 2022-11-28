966386b61dce731d45f24070166f8c4717efefdd
Kaloyan Nikolov <knikolovv98@gmail.com>
2022-11-28 13:23:50 +0200

    Fixes

--- database/migrations/2022_10_13_141618_create_custom_trades_table.php
+++ database/migrations/2022_10_13_141618_create_custom_trades_table.php
@@ -17,7 +17,7 @@ class CreateCustomTradesTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->index('user-id-custom-trades-idx');
             $table->foreignId('api_pair_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->index('pair-id-custom-trades-idx');
-            $table->text('symbol')->index('symbol-custom-trades-idx')->nullable()->default(null);
+            $table->text('symbol')->nullable()->default(null);
             $table->longText('buy_order_id')->nullable()->default(null);
             $table->longText('sell_order_id')->nullable()->default(null);
             $table->longText('order_list_id')->nullable()->default(null);
